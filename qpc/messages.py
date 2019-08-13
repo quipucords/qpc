386435869c12bd6d8cc44fc0d6a84fe830c53181
abaiken <aaiken@redhat.com>
2019-08-13 08:21:44 -0400

    add satellite to the list of valid values

--- qpc/messages.py
+++ qpc/messages.py
@@ -60,7 +60,7 @@ CRED_BECOME_PASSWORD_HELP = 'The privilege escalation password to be '\
 
 SOURCE_NAME_HELP = 'Source name.'
 SOURCES_NAME_HELP = 'List of source names.'
-SOURCE_TYPE_HELP = 'Type of source. Valid values: vcenter, network.'
+SOURCE_TYPE_HELP = 'Type of source. Valid values: vcenter, network, satellite.'
 SOURCE_HOSTS_HELP = 'IP ranges to scan. Run the "man %s" command for more '\
     'information about supported formats.'
 SOURCE_EXCLUDE_HOSTS_HELP = 'IP ranges to exclude from scan. Only supported '\

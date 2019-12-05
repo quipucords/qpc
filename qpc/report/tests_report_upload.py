d9dbe28e2647384f72f836b19159935149052721
Kevan Holdaway <kholdawa@redhat.com>
2019-12-05 15:18:30 -0500

    Remove dead code

--- qpc/report/tests_report_upload.py
+++ qpc/report/tests_report_upload.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018-2019 Red Hat, Inc.
+# Copyright (c) 2019 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public License,
 # version 3 (GPLv3). There is NO WARRANTY for this software, express or
@@ -25,28 +25,13 @@ from qpc.utils import get_server_location, write_server_config
 
 import requests_mock
 
-TMP_DETAILSFILE1 = ('/tmp/testdetailsreport1.json',
-                    '{"id": 1,"sources":[{"facts": ["AB"],"server_id": "8"}]}')
-TMP_DETAILSFILE2 = ('/tmp/testdetailsreport2.json',
-                    '{"id": 2, \n "sources": [{"source_name": "source2"}]}')
-TMP_NOTJSONFILE = ('/tmp/testnotjson.txt',
-                   'not a json file')
 TMP_BADDETAILS1 = ('/tmp/testbaddetailsreport_source.json',
                    '{"id": 4,"bsources":[{"facts": ["A"],"server_id": "8"}]}')
-TMP_BADDETAILS2 = ('/tmp/testbadetailsreport_facts.json',
-                   '{"id": 4,"sources":[{"bfacts": ["A"],"server_id": "8"}]}')
-TMP_BADDETAILS3 = ('/tmp/testbaddetailsreport_server_id.json',
-                   '{"id": 4,"sources":[{"facts": ["A"],"bserver_id": "8"}]}')
-TMP_BADDETAILS4 = ('/tmp/testbaddetailsreport_bad_json.json',
-                   '{"id":3,"sources"[this is bad]')
-TMP_BADDETAILS5 = ('/tmp/testbaddetailsinvalidreporttype.json',
-                   '{"report_type": "durham"}')
 TMP_GOODDETAILS = ('/tmp/testgooddetailsreport.json',
                    '{"id": 4,"sources":[{"facts": ["A"],"server_id": "8"}]}')
 NONEXIST_FILE = ('/tmp/does/not/exist/bad.json')
-JSON_FILES_LIST = [TMP_DETAILSFILE1, TMP_DETAILSFILE2, TMP_NOTJSONFILE,
-                   TMP_BADDETAILS1, TMP_BADDETAILS2, TMP_BADDETAILS3,
-                   TMP_GOODDETAILS, TMP_BADDETAILS5]
+JSON_FILES_LIST = [TMP_BADDETAILS1,
+                   TMP_GOODDETAILS]
 PARSER = ArgumentParser()
 SUBPARSER = PARSER.add_subparsers(dest='subcommand')
 
@@ -61,8 +46,6 @@ class ReportUploadTests(unittest.TestCase):
         # Temporarily disable stderr for these tests, CLI errors clutter up
         # nosetests command.
         self.orig_stderr = sys.stderr
-        self.test_json_filename = 'test_%d.json' % time.time()
-        self.test_csv_filename = 'test_%d.csv' % time.time()
         sys.stderr = HushUpStderr()
         for file in JSON_FILES_LIST:
             if os.path.isfile(file[0]):
@@ -77,14 +60,6 @@ class ReportUploadTests(unittest.TestCase):
             if os.path.isfile(file[0]):
                 os.remove(file[0])
         sys.stderr = self.orig_stderr
-        try:
-            os.remove(self.test_json_filename)
-        except FileNotFoundError:
-            pass
-        try:
-            os.remove(self.test_csv_filename)
-        except FileNotFoundError:
-            pass
 
     def test_upload_good_details_report(self):
         """Test uploading a good details report."""

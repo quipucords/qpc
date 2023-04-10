e73f38a3c5c28dd7e8a4a079b244d7d35785ffa6
Bruno Ciconelle <bciconel@redhat.com>
2023-04-10 16:17:57 -0300

    chore: update messages

--- qpc/cred/utils.py
+++ qpc/cred/utils.py
@@ -40,7 +40,7 @@ def get_password(args, req_payload, add_none=True):
     elif add_none:
         req_payload["become_password"] = None
     if getattr(args, "token", None):
-        token_prompt = getpass(messages.OPENSHIFT_TOKEN)
+        token_prompt = getpass(messages.AUTH_TOKEN)
         check_if_prompt_is_not_empty(token_prompt)
         req_payload["auth_token"] = token_prompt
     elif add_none:

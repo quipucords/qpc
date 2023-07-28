532d89fc6dfdaab74db3e5d00027b3f34a9befb9
Brad Smith <brasmith@redhat.com>
2023-07-28 14:44:58 -0400

    fix: fix failing `python setup.py install`
    This fix supports Red Hat's downstream build process which uses
    `python setup.py install` to install qpc. Before this change,
    `setup.py` failed because setting the version attempted to load
    package metadata before the package was installed. This change
    handles missing metadata by reading `pyproject.toml` directly to
    get the `tool.poetry.version` value.

--- qpc/__init__.py
+++ qpc/__init__.py
@@ -1,6 +1,27 @@
 """QPC Package Initialization."""
 from importlib import metadata
+from pathlib import Path
+from tomllib import load
 
-# Let's get the package version from poetry
-__package__version__ = metadata.version(__package__)
+try:
+    # Let's get the package version from poetry
+    __package__version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # PackageNotFoundError may be raised if *not* called from
+    # within a poetry environment, such as during execution of
+    # `python setup.py install` in downstream builds.
+    # As a failsafe, try to load the version defined for poetry
+    # from the pyproject.toml.
+    toml_path = Path(__file__).absolute().parent.parent / "pyproject.toml"
+    with open(toml_path, "rb") as f:
+        toml_data = load(f)
+        __package__version__ = (
+            # Expect a value at this exact location.
+            # Yes, this will raise KeyError if absent.
+            # We want to know ASAP if this breaks.
+            toml_data["tool"]["poetry"]["version"]
+        )
+
+del load
+del Path
 del metadata

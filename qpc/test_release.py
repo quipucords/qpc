39e9e987104ea2949be4fb9118107ca709cd3669
Brad Smith <brasmith@redhat.com>
2023-10-12 13:25:59 -0400

    refactor: drop gitpython, run git directly to get current repo sha1
    Relates to JIRA: DISCOVERY-403
    https://issues.redhat.com/browse/DISCOVERY-403

--- qpc/test_release.py
+++ qpc/test_release.py
@@ -1,4 +1,5 @@
 """Test the `qpc.release` module."""
+import subprocess
 from unittest import mock
 
 from qpc import release
@@ -12,23 +13,31 @@ def test_get_current_sha1_uses_env_var():
     assert actual_value == expected_value, "failed to get value from environment"
 
 
-def test_get_current_sha1_uses_git():
-    """Test getting the actual current from git."""
+@mock.patch("qpc.release.subprocess.run")
+@mock.patch.dict(release.os.environ, {"QPC_COMMIT": ""})
+def test_get_current_sha1_uses_git(mock_run):
+    """Test getting the actual current commit from git."""
     expected_value = "DECAFBAD"
-    with mock.patch.dict(release.os.environ, {"QPC_COMMIT": ""}), mock.patch(
-        "git.Repo.rev_parse"
-    ) as mock_rev_parse:
-        mock_rev_parse.return_value.hexsha = expected_value
-        actual_value = release.get_current_sha1()
+    mock_run.return_value.stdout = expected_value.encode()
+    actual_value = release.get_current_sha1()
     assert actual_value == expected_value, "failed to get SHA-1 value from git"
 
 
-def test_get_current_sha1_unknown():
+@mock.patch("qpc.release.subprocess.run")
+@mock.patch.dict(release.os.environ, {"QPC_COMMIT": ""})
+def test_get_current_sha1_unknown_no_git_repo(mock_run):
     """Test trying to get the SHA-1 when the env var and git repo are both missing."""
     expected_value = "UNKNOWN"
-    with mock.patch.dict(release.os.environ, {"QPC_COMMIT": ""}), mock.patch(
-        "git.Repo"
-    ) as mock_repo_class:
-        mock_repo_class.side_effect = release.git.exc.InvalidGitRepositoryError
-        actual_value = release.get_current_sha1()
+    mock_run.side_effect = subprocess.CalledProcessError(returncode=420, cmd="git")
+    actual_value = release.get_current_sha1()
+    assert actual_value == expected_value, "failed to get UNKNOWN value"
+
+
+@mock.patch("qpc.release.subprocess.run")
+@mock.patch.dict(release.os.environ, {"QPC_COMMIT": ""})
+def test_get_current_sha1_unknown_unexpected_git_stdout(mock_run):
+    """Test trying to get the SHA-1 when the git outputs a non-hexadecimal value."""
+    expected_value = "UNKNOWN"
+    mock_run.return_value.stdout = "this is not hexadecimal".encode()
+    actual_value = release.get_current_sha1()
     assert actual_value == expected_value, "failed to get UNKNOWN value"

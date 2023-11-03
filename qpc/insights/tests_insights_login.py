d5f501f48837890598b81a7780346a12efe4c914
Alberto Bellotti <abellott@redhat.com>
2023-11-03 16:29:17 -0400

    feat(insights): Add device authorization workflow
    - Add the insights device authorization workflow
    - updated the insights login command to request an authorization workflow
      and wait for the user to login/authorize out-of-band.
    - fixed an issue with writing the auth token to file.

--- qpc/insights/tests_insights_login.py
+++ qpc/insights/tests_insights_login.py
@@ -1,17 +1,5 @@
 """Test the CLI module."""
 
-import sys
-
-from qpc.cli import CLI
-
 
 class TestInsightsLogin:
     """Class for testing insights login command."""
-
-    def test_insight_login(self, capsys):
-        """Testing if insights login command requires args."""
-        sys.argv = ["/bin/qpc", "insights", "login"]
-        CLI().main()
-        out, err = capsys.readouterr()
-        assert out == ""
-        assert err == ""

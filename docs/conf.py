e6aa9c391e4727588552721792a8e6d95e72c765
Brad Smith <brasmith@redhat.com>
2023-11-10 15:48:30 -0500

    docs: add standard sphinx makefile and conf.py
    Relates to JIRA: DISCOVERY-403
    https://issues.redhat.com/browse/DISCOVERY-403

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# project information
+project = 'Quipucords'
+#copyright = '2023, Red Hat'
+#author = 'Red Hat'
+release = '1.4.z'
+
+# general configuration
+extensions = []
+exclude_patterns = ['_build']
+docs/make.bat
+
+
+# man page output options
+man_make_section_directory = False
+man_pages = [['index', 'qpc', None, None, 1],]

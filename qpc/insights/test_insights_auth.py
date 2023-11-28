15281c6893b53e8e5a6119a095963d002cafd0bb
Alberto Bellotti <abellott@redhat.com>
2023-11-27 19:49:48 -0500

    feat(insights): Dynamically query the device auth and token endpoints
    - While these have not changed for a long time, it is always a good idea
      to fetch the device_authorization_endpoint and token_endpoint from
      the Keycloak OpenID well-known configuration json.

--- qpc/insights/test_insights_auth.py
+++ qpc/insights/test_insights_auth.py
@@ -4,10 +4,24 @@ import http
 from unittest.mock import patch
 
 import pytest
+from faker import Faker
 
-from qpc.insights.auth import DEVICE_AUTH_ENDPOINT, TOKEN_ENDPOINT, InsightsAuth
+from qpc.insights.auth import (
+    DEVICE_AUTH_ENDPOINT_KEY,
+    OPENID_CONFIG_ENDPOINT,
+    TOKEN_ENDPOINT_KEY,
+    InsightsAuth,
+)
 from qpc.insights.exceptions import InsightsAuthError
 from qpc.utils import CONFIG_SSO_HOST_KEY, read_insights_config, write_insights_config
+
+fake = Faker()
+
+
+def get_sso_config_url():
+    """Return the Insights SSO Configuration endpoint."""
+    sso_host = read_insights_config().get(CONFIG_SSO_HOST_KEY)
+    return f"https://{sso_host}{OPENID_CONFIG_ENDPOINT}"
 
 
 class TestInsightsAuth:
@@ -38,19 +52,20 @@ class TestInsightsAuth:
         """Return an alternate staging sso host."""
         return f"stage-{faker.hostname()}"
 
-    def test_insights_request_auth(self, login_auth_response, faker, requests_mock):
-        """Testing that insights auth targets the right endpoint."""
-        sso_host = read_insights_config().get(CONFIG_SSO_HOST_KEY)
-        sso_url = f"https://{sso_host}{DEVICE_AUTH_ENDPOINT}"
-        requests_mock.post(sso_url, json=login_auth_response)
-        assert InsightsAuth().request_auth() == login_auth_response
-
-    def test_insights_request_auth_alt_sso_host(
-        self, stage_sso_host, login_auth_response, faker, requests_mock
+    @pytest.mark.parametrize(
+        "alt_sso_host",
+        [None, f"stage-{fake.hostname()}"],
+        ids=["default_sso_host", "stage_sso_host"],
+    )
+    def test_insights_request_auth(
+        self, login_auth_response, faker, requests_mock, alt_sso_host
     ):
-        """Testing that insights auth targets alternate sso hosts."""
-        write_insights_config({CONFIG_SSO_HOST_KEY: stage_sso_host})
-        sso_url = f"https://{stage_sso_host}{DEVICE_AUTH_ENDPOINT}"
+        """Testing that insights auth targets the right endpoint."""
+        if alt_sso_host:
+            write_insights_config({CONFIG_SSO_HOST_KEY: alt_sso_host})
+        sso_url = faker.url()
+        sso_config = {DEVICE_AUTH_ENDPOINT_KEY: sso_url}
+        requests_mock.get(get_sso_config_url(), json=sso_config)
         requests_mock.post(sso_url, json=login_auth_response)
         assert InsightsAuth().request_auth() == login_auth_response
 
@@ -59,28 +74,35 @@ class TestInsightsAuth:
         write_insights_config({CONFIG_SSO_HOST_KEY: stage_sso_host})
         with pytest.raises(InsightsAuthError) as err:
             InsightsAuth().request_auth()
-            assert "Failed to request login authorization" in err.value
+        assert "Failed to request login authorization" in str(err.value)
 
+    def test_insights_request_auth_invalid_sso_config(self, faker, requests_mock):
+        """Testing that insights auth handles invalid SSO configuration errors."""
+        with pytest.raises(InsightsAuthError) as err:
+            sso_config = {faker.slug(): faker.hostname()}
+            requests_mock.get(get_sso_config_url(), json=sso_config)
+            InsightsAuth().request_auth()
+        assert (
+            "Failed to query the Insights SSO configuration:"
+            f" missing {DEVICE_AUTH_ENDPOINT_KEY}" in str(err.value)
+        )
+
+    @pytest.mark.parametrize(
+        "alt_sso_host",
+        [None, f"stage-{fake.hostname()}"],
+        ids=["default_sso_host", "stage_sso_host"],
+    )
     def test_insights_wait_for_authorization_authorized(
-        self, login_auth_response, auth_token, requests_mock
+        self, login_auth_response, auth_token, faker, requests_mock, alt_sso_host
     ):
         """Testing that insights wait for authorization returns the auth token."""
+        if alt_sso_host:
+            write_insights_config({CONFIG_SSO_HOST_KEY: alt_sso_host})
         insights_auth = InsightsAuth()
         insights_auth.auth_request = login_auth_response
-        sso_host = read_insights_config().get(CONFIG_SSO_HOST_KEY)
-        sso_token_url = f"https://{sso_host}{TOKEN_ENDPOINT}"
-        token_response = {"access_token": auth_token}
-        requests_mock.post(sso_token_url, json=token_response)
-        assert insights_auth.wait_for_authorization() == auth_token
-
-    def test_insights_wait_for_authorization_authorized_alt_sso_host(
-        self, stage_sso_host, login_auth_response, auth_token, requests_mock
-    ):
-        """Testing that insights wait for authorization handles connection errors."""
-        write_insights_config({CONFIG_SSO_HOST_KEY: stage_sso_host})
-        insights_auth = InsightsAuth()
-        insights_auth.auth_request = login_auth_response
-        sso_token_url = f"https://{stage_sso_host}{TOKEN_ENDPOINT}"
+        sso_token_url = faker.url()
+        sso_config = {TOKEN_ENDPOINT_KEY: sso_token_url}
+        requests_mock.get(get_sso_config_url(), json=sso_config)
         token_response = {"access_token": auth_token}
         requests_mock.post(sso_token_url, json=token_response)
         assert insights_auth.wait_for_authorization() == auth_token
@@ -94,26 +116,29 @@ class TestInsightsAuth:
         insights_auth.auth_request = login_auth_response
         with pytest.raises(InsightsAuthError) as err:
             insights_auth.wait_for_authorization()
-            assert "Failed to verify Login authorization" in err.value
+        assert "Failed to verify Login authorization" in str(err.value)
 
     def test_insights_wait_for_authorization_bad_request(
-        self, login_auth_response, auth_token, requests_mock
+        self, login_auth_response, auth_token, faker, requests_mock
     ):
         """Testing that wait for authorization fails with failed checks."""
         insights_auth = InsightsAuth()
         insights_auth.auth_request = login_auth_response
-        sso_host = read_insights_config().get(CONFIG_SSO_HOST_KEY)
-        sso_token_url = f"https://{sso_host}{TOKEN_ENDPOINT}"
+        sso_token_url = faker.url()
+        sso_config = {TOKEN_ENDPOINT_KEY: sso_token_url}
+        requests_mock.get(get_sso_config_url(), json=sso_config)
         token_response = {"error": "unknown_error"}
         requests_mock.post(
             sso_token_url, status_code=http.HTTPStatus.BAD_REQUEST, json=token_response
         )
         with pytest.raises(InsightsAuthError) as err:
             insights_auth.wait_for_authorization()
-            assert "Failed to verify Login authorization" in err.value
+        assert "Failed to verify Login authorization" in str(err.value)
 
     @patch("time.sleep")
-    def test_insights_wait_for_authorization_expired(self, faker, requests_mock):
+    def test_insights_wait_for_authorization_expired(
+        self, mock_sleep, faker, requests_mock
+    ):
         """Testing that authorization pending checks for expired time."""
         insights_auth = InsightsAuth()
         insights_auth.auth_request = {
@@ -121,11 +146,13 @@ class TestInsightsAuth:
             "user_code": faker.slug(),
             "verification_uri": faker.url(),
             "verification_uri_complete": faker.url(),
-            "expires_in": 1,
-            "interval": 0,
+            "expires_in": 0,
+            "interval": 1,
         }
-        sso_host = read_insights_config().get(CONFIG_SSO_HOST_KEY)
-        sso_token_url = f"https://{sso_host}{TOKEN_ENDPOINT}"
+        mock_sleep.return_value = None
+        sso_token_url = faker.url()
+        sso_config = {TOKEN_ENDPOINT_KEY: sso_token_url}
+        requests_mock.get(get_sso_config_url(), json=sso_config)
         token_response = {
             "error": "authorization_pending",
             "error_description": "The authorization request is still pending",
@@ -135,16 +162,17 @@ class TestInsightsAuth:
         )
         with pytest.raises(InsightsAuthError) as err:
             insights_auth.wait_for_authorization()
-            assert "Time-out while waiting for Login authorization" in err.value
+        assert "Time-out while waiting for Login authorization" in str(err.value)
 
     def test_insights_wait_for_authorization_expired_from_sso(
-        self, login_auth_response, requests_mock
+        self, login_auth_response, faker, requests_mock
     ):
         """Testing that token authorization expired from the sso server."""
         insights_auth = InsightsAuth()
         insights_auth.auth_request = login_auth_response
-        sso_host = read_insights_config().get(CONFIG_SSO_HOST_KEY)
-        sso_token_url = f"https://{sso_host}{TOKEN_ENDPOINT}"
+        sso_token_url = faker.url()
+        sso_config = {TOKEN_ENDPOINT_KEY: sso_token_url}
+        requests_mock.get(get_sso_config_url(), json=sso_config)
         token_response = {
             "error": "expired_token",
             "error_description": "Device code is expired",
@@ -154,4 +182,4 @@ class TestInsightsAuth:
         )
         with pytest.raises(InsightsAuthError) as err:
             insights_auth.wait_for_authorization()
-            assert "Time-out while waiting for Login authorization" in err.value
+        assert "Time-out while waiting for Login authorization" in str(err.value)

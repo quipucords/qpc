469336866f93f78df444f1641b12dd7293f9b705
Bruno FS Ciconelle <bciconel@redhat.com>
2024-04-29 17:36:02 -0300

    refactor(scan)!: drop restart/pause functionality
    These funcionality won't be supported soon. Get rid of it.
    
    Relates to Jira: DISCOVERY-390

--- qpc/scan/commands.py
+++ qpc/scan/commands.py
@@ -6,7 +6,5 @@ from qpc.scan.clear import ScanClearCommand
 from qpc.scan.edit import ScanEditCommand
 from qpc.scan.job import ScanJobCommand
 from qpc.scan.list import ScanListCommand
-from qpc.scan.pause import ScanPauseCommand
-from qpc.scan.restart import ScanRestartCommand
 from qpc.scan.show import ScanShowCommand
 from qpc.scan.start import ScanStartCommand

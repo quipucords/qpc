d48b4d01a7ee025e6297a9efe25a0e8261cbb81c
Bruno FS Ciconelle <bciconel@redhat.com>
2024-05-13 11:56:05 -0300

    chore: bump version to 1.8.0
    The latest refactor on merge and upload report apis require discovery
    1.8.0 or greater.
    
    Relates to JIRA: DISCOVERY-672

--- qpc/utils.py
+++ qpc/utils.py
@@ -51,7 +51,7 @@ DEFAULT_INSIGHTS_CONFIG = {
 
 LOG_LEVEL_INFO = 0
 
-QPC_MIN_SERVER_VERSION = "1.6.0"
+QPC_MIN_SERVER_VERSION = "1.8.0"
 
 logging.captureWarnings(True)
 logger = logging.getLogger(__name__)

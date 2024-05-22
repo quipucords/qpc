e44aa14cb9e07a758d3c138729645ea0331b728f
Alberto Bellotti <abellott@redhat.com>
2024-05-22 17:11:22 -0400

    chore: Adding support for Fedora 40
    - Add Fedora 40 builds
    - Pulling in setuptools 70.0.0

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 from qpc.release import (
-    AUTHOR,
-    AUTHOR_EMAIL,
     ENTRYPOINT,
     URL,
     VERSION,
@@ -20,14 +18,7 @@ sys.path.insert(0, BASE_QPC_DIR / "qpc")
 setup(
     name="qpc",
     version=VERSION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
     include_package_data=True,
-    license="GPLv3",
     packages=find_packages(
         exclude=[
             "**/test_*.py",

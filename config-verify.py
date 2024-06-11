42893057f46158611917b7eaa955d4710204338d
Bruno FS Ciconelle <bciconel@redhat.com>
2024-06-11 13:59:21 -0300

    chore(config-verify): also compare with qpc.spec version

--- config-verify.py
+++ config-verify.py
@@ -1,8 +1,16 @@
-"""QPC pyproject toml file verification."""
+"""QPC pyproject toml and spec file verification."""
 
+import re
 import sys
 from pathlib import Path
 from tomllib import load
+
+spec_path = Path(__file__).absolute().parent / "qpc.spec"
+match_version = re.search(r"Version:\s+(\d+\.\d+\.\d+)", spec_path.read_text())
+if not match_version:
+    print("Unable to parse version on qpc.spec", file=sys.stderr)
+    sys.exit(1)
+spec_version = match_version.groups()[0]
 
 toml_path = Path(__file__).absolute().parent / "pyproject.toml"
 with toml_path.open("rb") as toml_file:
@@ -19,10 +27,11 @@ with toml_path.open("rb") as toml_file:
         )
         sys.exit(1)
 
-    if project["version"] != tool_poetry["version"]:
+    if spec_version != project["version"] or spec_version != tool_poetry["version"]:
         print(
-            "Project version {v1} does not match tool.poetry version {v2}".format(
-                v1=project["version"], v2=tool_poetry["version"]
+            "Versions {v1} (project) and {v2} (tool.poetry) in pyproject.toml, and "
+            "{v3} in qpc.spec are inconsistent.".format(
+                v1=project["version"], v2=tool_poetry["version"], v3=spec_version
             ),
             file=sys.stderr,
         )

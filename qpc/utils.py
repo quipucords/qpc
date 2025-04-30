8fc37117c09185974aca2e674b5d9ccb882f0aa7
Bruno Ciconelle <bciconel@redhat.com>
2025-04-30 07:00:57 -0300

    chore: bump qpc to 1.14.0

--- qpc/utils.py
+++ qpc/utils.py
@@ -54,7 +54,7 @@ DEFAULT_INSIGHTS_CONFIG = {
 
 LOG_LEVEL_INFO = 0
 
-QPC_MIN_SERVER_VERSION = "1.13.1"
+QPC_MIN_SERVER_VERSION = "1.14.0"
 
 logging.captureWarnings(True)
 logger = logging.getLogger(__name__)

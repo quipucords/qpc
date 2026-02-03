3735ed281f8eb8e8f06b708008d8a1e1bada056f
abellotti <abellott@redhat.com>
2026-02-03 15:56:49 -0500

    feat: Add support for qpc report list
    - Adding support for qpc report list subcommand
      to exercise the /api/v2/reports/ endpoint.

--- qpc/report/commands.py
+++ qpc/report/commands.py
@@ -5,6 +5,7 @@ from qpc.report.deployments import ReportDeploymentsCommand
 from qpc.report.details import ReportDetailsCommand
 from qpc.report.download import ReportDownloadCommand
 from qpc.report.insights import ReportInsightsCommand
+from qpc.report.list import ReportListCommand
 from qpc.report.merge import ReportMergeCommand
 from qpc.report.show import ReportShowCommand
 from qpc.report.upload import ReportUploadCommand

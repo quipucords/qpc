2cfec52f3608e91f115a8aacbbb201ccbc177d34
abellotti <abellott@redhat.com>
2026-02-03 15:56:49 -0500

    feat: Add support for qpc report list
    - Adding support for qpc report list subcommand
      to exercise the /api/v2/reports/ endpoint.

---
+++ qpc/report/list.py
@@ -0,0 +1,76 @@
+"""ReportListCommand is used to list available reports."""
+
+import json
+import sys
+from logging import getLogger
+
+from requests import codes
+
+from qpc import messages, report
+from qpc.clicommand import CliCommand
+from qpc.request import GET
+from qpc.translation import _
+from qpc.utils import (
+    check_extension,
+    pretty_format,
+    validate_write_file,
+    write_file,
+)
+
+logger = getLogger(__name__)
+
+
+class ReportListCommand(CliCommand):
+    """Defines the report list command.
+
+    This command is for listing available reports.
+    """
+
+    SUBCOMMAND = report.SUBCOMMAND
+    ACTION = report.LIST
+
+    def __init__(self, subparsers):
+        """Create command."""
+        CliCommand.__init__(
+            self,
+            self.SUBCOMMAND,
+            self.ACTION,
+            subparsers.add_parser(self.ACTION),
+            GET,
+            report.REPORT_V2_URI,
+            [codes.ok],
+        )
+
+        self.parser.add_argument(
+            "--output-file",
+            dest="path",
+            metavar="PATH",
+            help=_(messages.REPORT_PATH_HELP),
+        )
+        self.min_server_version = "0.9.2"
+
+    def _validate_args(self):  # noqa: PLR0912
+        CliCommand._validate_args(self)
+        extension = ".json"
+        self.req_headers = {"Accept": "application/json"}
+        check_extension(extension, self.args.path)
+        try:
+            if self.args.path:
+                validate_write_file(self.args.path, "output-file")
+        except ValueError as error:
+            logger.error(error)
+            sys.exit(1)
+
+        self.req_path = f"{self.req_path}"
+
+    def _handle_response_success(self):
+        try:
+            json_data = json.loads(self.response.text)
+            response_json = pretty_format(json_data)
+            write_file(self.args.path, response_json)
+            logger.info(_(messages.REPORT_SUCCESSFULLY_WRITTEN))
+        except EnvironmentError as err:
+            logger.error(
+                _(messages.WRITE_FILE_ERROR), {"path": self.args.path, "error": err}
+            )
+            sys.exit(1)

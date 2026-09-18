0a0d752e0915ff0bf6538c08e54326284969444e
Mirek Długosz <mzalewsk@redhat.com>
2026-09-18 17:30:00 +0200

    Update MIN_SERVER_VERSION with make bump-version
    `make bump-version` should also update `QPC_MIN_SERVER_VERSION` in
    qpc/utils.py - at least if major or minor version changed.

--- get-version.py
+++ get-version.py
@@ -1,10 +1,24 @@
 """Get qpc's version from pyproject.toml."""
 
+import sys
 import tomllib
 from pathlib import Path
+
+major_minor_zero = False
+
+try:
+    major_minor_zero = sys.argv[1] == "--major-minor"
+except IndexError:
+    pass
 
 toml_path = Path(__file__).absolute().parent / "pyproject.toml"
 with toml_path.open("rb") as fp:
     data = tomllib.load(fp)
 
-print(data["project"]["version"])
+output_version = data["project"]["version"]
+
+if major_minor_zero:
+    version, _, _ = output_version.rpartition(".")
+    output_version = f"{version}.0"
+
+print(output_version)
